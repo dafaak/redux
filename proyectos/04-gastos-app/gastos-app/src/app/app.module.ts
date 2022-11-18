a979ef045b955bb76c1226d3311712a280d97014
Israel <israel.pe@outlook.es>
2022-11-18 16:07:08 -0500

    update: register

--- proyectos/04-gastos-app/gastos-app/src/app/app.module.ts
+++ proyectos/04-gastos-app/gastos-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 // Modulos
 import {AppRoutingModule} from './app-routing.module';
+import {ReactiveFormsModule} from "@angular/forms";
 
 // Componentes
 import {AppComponent} from './app.component';
@@ -15,6 +16,7 @@ import {DetalleComponent} from './ingreso-egreso/detalle/detalle.component';
 import {FooterComponent} from './shared/footer/footer.component';
 import {NavbarComponent} from './shared/navbar/navbar.component';
 import {SidebarComponent} from './shared/sidebar/sidebar.component';
+
 
 @NgModule({
   declarations: [
@@ -31,7 +33,8 @@ import {SidebarComponent} from './shared/sidebar/sidebar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- proyectos/04-gastos-app/gastos-app/src/app/auth/register/register.component.ts
+++ proyectos/04-gastos-app/gastos-app/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formGroup: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.formGroup = this.fb.group(
+      {
+        nombre: ['', [Validators.required]],
+        correo: ['', [Validators.email, Validators.required]],
+        password: ['', [Validators.required]],
+
+      }
+    )
+  }
 
   ngOnInit(): void {
   }
 
+  crearUsuario() {
+    console.log(this.formGroup);
+    console.log(this.formGroup.valid);
+    console.log(this.formGroup.value);
+  }
 }
