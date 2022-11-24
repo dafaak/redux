702d00e372e14f554b7756caec9e0788af044f46
Israel <israel.pe@outlook.es>
2022-11-24 15:29:39 -0500

    feat: ingreso egreso module

--- proyectos/04-gastos-app/gastos-app/src/app/app.module.ts
+++ proyectos/04-gastos-app/gastos-app/src/app/app.module.ts
@@ -12,40 +12,28 @@ import {AngularFireAuthModule} from "@angular/fire/compat/auth";
 import {StoreModule} from '@ngrx/store';
 // Componentes
 import {AppComponent} from './app.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {IngresoEgresoComponent} from './ingreso-egreso/ingreso-egreso.component';
-import {EstadisticaComponent} from './ingreso-egreso/estadistica/estadistica.component';
-import {DetalleComponent} from './ingreso-egreso/detalle/detalle.component';
-import {FooterComponent} from './shared/footer/footer.component';
-import {NavbarComponent} from './shared/navbar/navbar.component';
-import {SidebarComponent} from './shared/sidebar/sidebar.component';
-import {OrdenIngresoPipe} from './ingreso-egreso/pipes/orden-ingreso.pipe';
 
 import {environment} from '../environments/environment';
 import {appReducers} from "./app.reducer";
 import {SharedModule} from "./shared/shared.module";
+import {IngresoEgresoModule} from "./ingreso-egreso/ingreso-egreso.module";
+import {CommonModule} from "@angular/common";
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
-    IngresoEgresoComponent,
-    EstadisticaComponent,
-    OrdenIngresoPipe,
-    DetalleComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     StoreModule.forRoot(appReducers, {}),
     StoreDevtoolsModule.instrument({maxAge: 25, logOnly: environment.production}),
-    NgChartsModule,
     AuthModule,
-    SharedModule,
+    IngresoEgresoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ proyectos/04-gastos-app/gastos-app/src/app/ingreso-egreso/ingreso-egreso.module.ts
@@ -0,0 +1,31 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from "@angular/forms";
+import {DashboardComponent} from "../dashboard/dashboard.component";
+import {IngresoEgresoComponent} from "./ingreso-egreso.component";
+import {EstadisticaComponent} from "./estadistica/estadistica.component";
+import {OrdenIngresoPipe} from "./pipes/orden-ingreso.pipe";
+import {DetalleComponent} from "./detalle/detalle.component";
+import {NgChartsModule} from "ng2-charts";
+import {SharedModule} from "../shared/shared.module";
+import {RouterModule} from "@angular/router";
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    IngresoEgresoComponent,
+    EstadisticaComponent,
+    OrdenIngresoPipe,
+    DetalleComponent,
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    NgChartsModule,
+    SharedModule,
+    RouterModule
+  ]
+})
+export class IngresoEgresoModule {
+}
