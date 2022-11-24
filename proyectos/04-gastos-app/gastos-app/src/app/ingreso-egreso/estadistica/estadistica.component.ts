ab63f0dcc607c95b26c09da8f47cbb07b7db31ab
Israel <israel.pe@outlook.es>
2022-11-24 12:22:57 -0500

    feat: estadisticas

--- proyectos/04-gastos-app/gastos-app/src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ proyectos/04-gastos-app/gastos-app/src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,15 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {AppState} from "../../app.reducer";
+import {IngresoEgresoModel} from "../../models/ingreso-egreso.model";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-estadistica',
   templateUrl: './estadistica.component.html',
   styleUrls: ['./estadistica.component.css']
 })
-export class EstadisticaComponent implements OnInit {
+export class EstadisticaComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  items: IngresoEgresoModel[] = [];
+  numeroIngresos = 0;
+  numeroEgresos = 0;
+  totalIngresos = 0;
+  totalEgresos = 0;
 
-  ngOnInit(): void {
+
+  itemsSubscription!: Subscription;
+
+  constructor(
+    private store: Store<AppState>
+  ) {
   }
 
+  ngOnInit(): void {
+    this.itemsSubscription = this.store.select(state => {
+      return state.ingresosEgresos;
+    })
+      .subscribe(
+        ({items}) => {
+          this.generarEstadistica(items);
+        }
+      )
+  }
+
+  ngOnDestroy() {
+    this.itemsSubscription.unsubscribe();
+  }
+
+  generarEstadistica(items: IngresoEgresoModel[]) {
+    this.numeroIngresos = 0;
+    this.totalIngresos = 0;
+    this.numeroEgresos = 0;
+    this.totalEgresos = 0;
+    items.forEach(item => {
+        if (item.tipo === 'ingreso') {
+          this.totalIngresos += item.monto;
+          this.numeroIngresos++;
+        }
+        if (item.tipo === 'egreso') {
+          this.totalEgresos += item.monto;
+          this.numeroEgresos++;
+        }
+      }
+    )
+  }
 }
